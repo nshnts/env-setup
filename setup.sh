f48b180ffe4b410db1262f8d6cabfdebcbfcf9cd
nsatyarthy <6873481+nsatyarthy@users.noreply.github.com>
2021-03-05 08:45:29 +0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+BASH_FILE=${HOME}/.my_bash
+EMACS_FILE=${HOME}/.emacs
+TMUX_FILE=${HOME}/.tmux.conf
+
+if [ ! -f "$BASH_FILE" ]; then
+    echo "Downloading .my_bash"
+    wget -P ${HOME} https://raw.githubusercontent.com/nsatyarthy/env-setup/main/.my_bash
+    echo "source ${HOME}/.my_bash" >> ${HOME}/.bashrc
+    echo "Done"
+fi
+
+if [ ! -f "$EMACS_FILE" ]; then
+    echo "Downloading .my_bash"
+    wget -P ${HOME} https://raw.githubusercontent.com/nsatyarthy/env-setup/main/.emacs
+    echo "Done"
+fi
+
+if [ ! -f "$TMUX_FILE" ]; then
+    echo "Downloading .my_bash"
+    wget -P ${HOME} https://raw.githubusercontent.com/nsatyarthy/env-setup/main/.tmux.conf
+    echo "Done"
+fi
